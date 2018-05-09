25b44218f855da58da9b18566b26d22c1be1f0d3
kyriosdata <fabio@inf.ufg.br>
2018-05-09 14:37:41 -0300

    elementos fora da especificacao

--- topico-4/Propriedade3025.js
+++ topico-4/Propriedade3025.js
@@ -5,8 +5,5 @@ function Propriedade3025(number) {
     const i = Math.trunc(number / 100);
     const j = Math.trunc(number % 100);
 
-    console.log(i, j);
     return Math.pow((i + j), 2) == number;
 }
-
-console.log(Propriedade3025(1))
